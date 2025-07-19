7a03f67e9adef30ddaafffcf570193fd60b7fe35
Manoj Prajapati <prmanoj2003@gmail.com>
2025-07-19 20:47:02 +0530

    Resolve build error

--- src/app/not-found.js
+++ src/app/not-found.js
@@ -8,7 +8,7 @@ export default function NotFound() {
                 <h1 className="text-7xl font-extrabold text-green-700 mb-6">404</h1>
                 <h2 className="text-3xl font-bold text-gray-800 mb-4">Page Not Found</h2>
                 <p className="text-gray-600 mb-8 text-lg">
-                    Oops! The page you're looking for doesn't exist or has been moved.
+                    Oops! The page you&apos;re looking for doesn&apos;t exist or has been moved.
                 </p>
                 <Link
                     href="/"
